6805b2eeb2d51d9c6f7bc5a7279a5e9503068821
kbnhvn <joel.lourenco.perso@gmail.com>
2024-05-22 22:53:21 +0000

    fix script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@
 
 terraform init -upgrade
 
-terraform apply -target=module.vpc -target=module.security_groups -target=module.iam -target=module.s3 -target=module.eks -target=module.alb -auto-approve
+terraform apply -auto-approve
 
-terraform apply -target=module.jenkins -target=module.traefik -auto-approve
